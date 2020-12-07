bfe27029795f518392d1db76c392473a25f431a7
Hannah Park <ha3158987@gmail.com>
2020-12-07 17:43:06 +0900

    Create skeleton code
    This is very first commit for new project.
    Create skeleton code for each file(html, css, js).

---
+++ index.js
@@ -0,0 +1,38 @@
+/*
+1단계: 단어 밀어내기
+1. 입력: 사용자로부터 단어 하나, 정수 숫자 하나(-100 <= N < 100), L 또는 R을 입력받는다. L 또는 R은 대소문자 모두 입력 가능하다.
+2. 주어진 단어를 L이면 주어진 숫자 갯수만큼 왼쪽으로, R이면 오른쪽으로 밀어낸다.
+3. 밀려나간 단어는 반대쪽으로 채워진다.
+
+***주의사항***
+- 컴파일 및 실행되지 않을 경우 불합격.
+- 자기만의 기준으로 최대한 간결하게 코드를 작성한다.
+*/
+class PushWord {
+
+    init() {
+        this.addClickEvent();
+    }
+
+    addClickEvent(){
+        const button = document.querySelector(".button_enter");
+
+        button.addEventListener("click", this.getInputValue);
+    }
+
+    getInputValue(){
+        const form = document.querySelector(".input_form");
+        const input = document.querySelector(".word");
+        const inputValue = input.value;
+
+        console.log(inputValue);
+        console.log(inputValue.split(" "));
+
+        form.reset();
+        input.focus();
+    }
+
+}
+
+const pushWordCalculator = new PushWord();
+pushWordCalculator.init();
